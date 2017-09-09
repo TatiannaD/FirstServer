a2d4cf5e6befe3e195578592b3e5910a0dd598f5
Tatianna <Tatianna@dhcp-v072-049.mobile.uci.edu>
2017-09-09 11:02:59 -0700

    first server final

---
+++ firstServer_js.js
@@ -0,0 +1,35 @@
+// Here we require/import the HTTP module
+var http = require("http");
+
+// Here we define a port to listen to
+var PORT = 7000;
+var PORT2 = 7500
+
+// Here we create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("It Works because Tatianna is a bad bish!! Path Hit: " + request.url);
+}
+
+// Here we use the Node HTTP package to create our server.
+// We then pass it the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Here we start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:", PORT);
+
+});
+
+function handleRequest2(request, response) {
+  response.end("I have nothing bad to say, because girl you bomb! Path Hit: " + request.url);
+}
+
+var server = http.createServer(handleRequest2);
+
+server.listen(PORT2, function() {
+  console.log("Server listening on: http://localhost:", PORT2);
+});
